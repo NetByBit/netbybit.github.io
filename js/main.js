616967db5d6aaee828585c0ed555efa418a9fe8a
MostaDaoud <30619697+MostaDaoud@users.noreply.github.com>
2017-08-12 13:00:58 +0200

    add loading

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ $(function () {
     var navbar = $('.navbar');
     $(window).scrollTop() >= navbar.height() ? navbar.addClass('scrolled') : navbar.removeClass('scrolled');
   });
-  // Deal With Tabs
-  $('.tab-switch li').click(function () {
-    // Add Selected Class To Active Link
-    $(this).addClass('selected').siblings().removeClass('selected');
-    // Hide All Divs
-    $('.tabs-section .tabs-content > div').hide();
-    // Show Div Connected With This Link
-    $($(this).data('class')).show();
-  });
+
+  $(window).on('load', () => {
+    $('.spinner-wrapper').fadeOut(500)
+    $('.responsive_headline').addClass('animated rubberBand')
+})
+
 });
