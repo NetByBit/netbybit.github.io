34f991fcb64e336733b492eb70875da984671ac3
MostaDaoud <30619697+MostaDaoud@users.noreply.github.com>
2017-08-12 15:03:43 +0200

    Animated Icons

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ $(function () {
   $(window).on('load', () => {
     $('.spinner-wrapper').fadeOut(500)
     $('.responsive_headline').addClass('animated rubberBand')
-})
+  })
+
+  // smooth scroll
+  $('.navbar-right li a').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#' + $(this).data('value')).offset().top
+    }, 1000);
+  });
 
 });
