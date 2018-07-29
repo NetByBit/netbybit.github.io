54d95ff72e314b373be1e8c9ee1cfc9406219f3e
Amr A. Mohamed <merodiro@gmail.com>
2018-07-29 13:09:10 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,19 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        // You can find your read-only API token under the Settings > API tokens
-        // section of your administrative area:
         apiToken: process.env.DATOCMS_APITOKEN,
-
-        // If you are working on development/staging environment, you might want to
-        // preview the latest version of records instead of the published one:
         previewMode: false,
-
-        // Disable automatic reloading of content when some change occurs on DatoCMS:
         disableLiveReload: false,
-
-        // Custom API base URL
         apiUrl: 'https://site-api.datocms.com',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-18396819-5",
+        head: false,
+        respectDNT: true,
       },
     },
     {
